7100576a571ce5e2c1a07d1606f66630554fb0c0
CODIFY <frankmwangi8118@gmail.com>
2025-07-11 11:37:37 +0300

    feat: binary search

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,9 @@ import java.lang.management.MemoryUsage;
 public class Main {
     public static void main(String[] args) {
        LinearSearch linearSearch = new LinearSearch();
+       BinarySearch binarySearch = new BinarySearch();
         int nums[]= {1,2,3,4,5,6};
         System.out.println(linearSearch.linear(nums,2));
+        System.out.println(binarySearch.binary(nums,1));
     }
 }
